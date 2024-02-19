26b4b6a9c74852fb5ce72a9a319623c7ee3e1b76
Yaxxir <160383327+Yaxxir@users.noreply.github.com>
2024-02-19 20:55:53 +0530

    Update info.py

--- info.py
+++ info.py
@@ -11,14 +11,14 @@ def is_enabled(value, default):
 
 
 # PyroClient Setup 
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
+API_ID = int(environ['20608057'])
+API_HASH = environ['56844fba51e9d1eb3edc516897749550 ']
+BOT_TOKEN = environ['6906428461:AAHrc4cRQYZCqTLBbmjweJaeqHsrWKfRlbk']
 
 # Bot settings
 WEBHOOK = bool(environ.get("WEBHOOK", True)) # for web support on/off
 PICS = (environ.get('PICS' ,'https://graph.org/file/01ddfcb1e8203879a63d7.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg https://graph.org/file/a125497b6b85a1d774394.jpg https://graph.org/file/43d26c54d37f4afb830f7.jpg https://graph.org/file/60c1adffc7cc2015f771c.jpg https://graph.org/file/d7b520240b00b7f083a24.jpg https://graph.org/file/0f336b0402db3f2a20037.jpg https://graph.org/file/39cc4e15cad4519d8e932.jpg https://graph.org/file/d59a1108b1ed1c6c6c144.jpg https://te.legra.ph/file/3a4a79f8d5955e64cbb8e.jpg https://graph.org/file/d69995d9846fd4ad632b8.jpg')).split()
-UPTIME = time.time()
+UPTIME = time.time(time)
 
 # Admins, Channels & Users
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
